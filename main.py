43d87e4c23b93e593f9dfdd4782fa5626ac3be0c
VictorZP <61043613+VictorZP@users.noreply.github.com>
2025-09-09 03:31:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -371,4 +371,8 @@ async def get_screenshot_proxy(url: str):
 # ===== локальный запуск =====
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
+    
